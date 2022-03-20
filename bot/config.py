d941f352bedbe8a32cd5a81bc0ff3cfd6e3cb1f1
gamer-6969 <78274246+gamer-6969@users.noreply.github.com>
2022-03-20 12:03:12 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,12 +3,12 @@ import json
 from distutils.util import strtobool as stb
 
 # --------------------------------------
-BOT_TOKEN = ""
-GDRIVE_FOLDER_ID = ""
+BOT_TOKEN = "5193719129:AAEaONG8Vdt7eflq4-LX6caJnF_OqfwG7wM"
+GDRIVE_FOLDER_ID = "1syrHfuhi8ENjNUEBv5VPoUo1PwQqA4ve"
 # Default folder id.
-OWNER_ID = 123455673
+OWNER_ID = 5058047318
 # Example: OWNER_ID = 619418070
-AUTHORISED_USERS = []
+AUTHORISED_USERS = [5058047318, 5193719129]
 # Example: AUTHORISED_USERS = [63055333, 100483029, -1003943959]
 INDEX_URL = ""
 IS_TEAM_DRIVE = True
